cc7843869bd6ca65cf4090e9942ac08ba8eb52cd
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2024-03-17 21:09:26 +0530

    mongoDB connected

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const dbConnect = require("./config/dbConnect");
 const dotenv = require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 4000;
+dbConnect();
+
 
 app.listen(PORT ,() =>{
     console.log(`Server is running on PORT ${PORT}`)
