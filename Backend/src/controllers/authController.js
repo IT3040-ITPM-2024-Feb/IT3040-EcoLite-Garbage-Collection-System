47d55e4c273230be7343c37466a780cf0bc1f7fa
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2024-03-20 10:40:28 +0530

    login Partially completed

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -18,7 +18,7 @@ const loginUser = async (req, res) => {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
     // Generate JWT token
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
     // Send token in the Authorization header with Bearer scheme
     res.status(200).json({ message: 'Login successful', token: `Bearer ${token}` });
   } catch (error) {
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { message } from "antd";
+import * as Yup from "yup";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [error, setError] = useState("");
 
-export default Login
+  const initialValues = { emailOrUsername: "", password: "" };
+
+  const validationSchema = Yup.object({
+    emailOrUsername: Yup.string().required("Username is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const handleSubmit = async (values, form) => {
+    try {
+      const { setSubmitting } = form; // Destructure setSubmitting from form
+  
+      const response = await axios.post(
+        "http://localhost:5000/users/login",
+        values
+      );
+  
+      console.log("Login successful");
+  
+      // Store only the token in local storage
+      window.localStorage.setItem("token", response.data.token);
+  
+      // Store the user id and role in local storage
+      window.localStorage.setItem("userId", response.data.user.id);
+      window.localStorage.setItem("role", response.data.user.role);
+  
+      // Set LoggedIn to true
+      window.localStorage.setItem("LoggedIn", true);
+  
+      // Redirect to the appropriate dashboard based on user role
+      window.location.href = "/";
+    } catch (error) {
+      console.error("Login failed", error);
+      if (error.response) {
+        message.error(error.response.data.message);
+        setError("Invalid username or password");
+      } else {
+        // Handle other types of errors
+        message.error("An error occurred while logging in.");
+        setError("An error occurred while logging in.");
+      }
+    } finally {
+      if (form) {
+        const { setSubmitting } = form; // Destructure setSubmitting from form if form exists
+        setSubmitting(false);
+      }
+    }
+  };
+  return (
+    <div>
+      <h1>Sign in</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            name="emailOrUsername"
+            placeholder="Username"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </div>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <button type="submit" style={{ margin: "16px 0" }}>
+          Sign In
+        </button>
+      </form>
+      {/* forgot password */}
+      <p style={{ textAlign: "center" }}>
+        <a href="/sendOTP" style={{ textDecoration: "none" }}>
+          Forgot password?
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
